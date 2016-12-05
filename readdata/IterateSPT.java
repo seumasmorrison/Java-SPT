e405ed7486d45423e1992dc1300301126b3f24b2
josephmacleod <13000146@uhi.ac.uk>
2016-12-05 13:02:07 +0000

    Create IterateSPT.java
    Iterates ReadSPT and StoreSPT accross each file in 'spt_files' directory

---
+++ readdata/IterateSPT.java
@@ -0,0 +1,29 @@
+package readdata;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.*;
+
+public class IterateSPT {
+  
+  public static void main(String[] args) throws IOException {
+    String directory_path = "E:\\Documents\\Object Oriented Programming\\James Morrison\\Example file read\\spt_files";
+    
+    File folder = new File(directory_path);
+    File[] listOfFiles = folder.listFiles();
+   
+    StoreSPT storeSPT = new StoreSPT();
+    
+    for (File file : listOfFiles) {
+      if (file.isFile()) {
+        //System.out.println(file.getPath());
+        ReadSPT spt_data = new ReadSPT(file.getPath());
+        String[] file_data = spt_data.OpenFile();
+        //System.out.println(file_data[7]);
+        
+      } 
+      
+    };
+    
+  }
+}

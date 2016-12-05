d7581c8f6dfb8ef90ee2167eae28387f232f2018
josephmacleod <13000146@uhi.ac.uk>
2016-12-05 13:00:39 +0000

    Create ReadSPT.java
    Reads first 12 lines from spt file

---
+++ readdata/ReadSPT.java
@@ -0,0 +1,36 @@
+package readdata;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.Arrays;
+
+public class ReadSPT{
+  
+  private String path;
+  
+  public ReadSPT(String file_path){
+    path = file_path;
+  }
+
+  public String[] OpenFile() throws IOException{
+    FileReader fr = new FileReader(path);
+    BufferedReader textReader = new BufferedReader(fr);
+    
+    int i;
+    
+    int textlisting = 12;
+    String[] lines = new String[textlisting];
+            
+       for (i=0; i < lines.length; i++) {
+          lines[i] = textReader.readLine();
+          
+       } 
+    System.out.println(Arrays.toString(lines));
+    
+    int num_data_records = 12;
+    String[] spt_data = new String[num_data_records];
+    return spt_data;
+    }
+    
+  }
